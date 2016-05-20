7cbc96b66c0090b506b2de55f2db5e8503438265
Marrony Neris <neris.marrony@gmail.com>
2016-05-20 20:16:06 +0000

    finally works

--- display.js
+++ display.js
@@ -8,30 +8,16 @@ var LCD_D5 = new Gpio(24, 'out');
 var LCD_D6 = new Gpio(23, 'out');
 var LCD_D7 = new Gpio(18, 'out');
 
-var LCD_CHR = true;
-var LCD_CMD = false;
+var LCD_CHR = 1;
+var LCD_CMD = 0;
        
 var LCD_LINE_1 = 0x80;
 var LCD_LINE_2 = 0xC0;
 
 lcdInit();
 
-var pin = new Gpio(2, 'out');
-
-  console.log('blah');
-	sleep.sleep(1);
-	pin.writeSync(1);
-	sleep.sleep(1);
-	pin.writeSync(0);
-	sleep.sleep(1);
-	pin.writeSync(1);
-	sleep.usleep(1);
-	pin.writeSync(0);
-	sleep.sleep(1);
-  console.log('blah2');
-
-//lcdString("A", LCD_LINE_1);
-//lcdString("Raspberry", LCD_LINE_2);
+lcdString("Hello", LCD_LINE_1);
+lcdString("Raspberry", LCD_LINE_2);
 
 //lcdDestroy();
 
@@ -60,20 +46,20 @@ function lcdDestroy() {
 function lcdByte(byte, mode) {
   console.log('lcdByte1', byte, mode);
     writeValueCB(LCD_RS, mode);
-    writeValueCB(LCD_D4, (byte & 0x10) === 0x10 ? true : false);
-    writeValueCB(LCD_D5, (byte & 0x20) === 0x20 ? true : false);
-    writeValueCB(LCD_D6, (byte & 0x40) === 0x40 ? true : false);
-    writeValueCB(LCD_D7, (byte & 0x80) === 0x80 ? true : false);
+    writeValueCB(LCD_D4, (byte & 0x10) === 0x10 ? 1 : 0);
+    writeValueCB(LCD_D5, (byte & 0x20) === 0x20 ? 1 : 0);
+    writeValueCB(LCD_D6, (byte & 0x40) === 0x40 ? 1 : 0);
+    writeValueCB(LCD_D7, (byte & 0x80) === 0x80 ? 1 : 0);
     //toggle enable pin
     sleep.usleep(100);
     writeValueCB(LCD_E, 1, 100);
     sleep.usleep(100);
     writeValueCB(LCD_E, 0, 100);
     sleep.usleep(100);
-    writeValueCB(LCD_D4, (byte & 0x01) === 0x01 ? true : false);
-    writeValueCB(LCD_D5, (byte & 0x02) === 0x02 ? true : false);
-    writeValueCB(LCD_D6, (byte & 0x04) === 0x04 ? true : false);
-    writeValueCB(LCD_D7, (byte & 0x08) === 0x08 ? true : false);
+    writeValueCB(LCD_D4, (byte & 0x01) === 0x01 ? 1 : 0);
+    writeValueCB(LCD_D5, (byte & 0x02) === 0x02 ? 1 : 0);
+    writeValueCB(LCD_D6, (byte & 0x04) === 0x04 ? 1 : 0);
+    writeValueCB(LCD_D7, (byte & 0x08) === 0x08 ? 1 : 0);
     //toggle enable pin
     sleep.usleep(100);
     writeValueCB(LCD_E, 1, 100);
@@ -96,6 +82,7 @@ function lcdString(str, line) {
 /////////////////////////////////
 
 function writeValueCB(port, value) {
-	port.writeSync(value);
+  console.log('write', port.gpio, value);
+  port.writeSync(value);
 }
 
